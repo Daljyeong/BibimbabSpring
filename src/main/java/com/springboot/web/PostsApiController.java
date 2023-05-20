85d21fce99c003a188b8a4e81f76dc54f40f5d96
Daljyeong <jiyoungm1022@gmail.com>
2023-05-20 16:27:54 +0900

    feat: Update & read posts (230514 homework)

--- src/main/java/com/springboot/web/PostsApiController.java
+++ src/main/java/com/springboot/web/PostsApiController.java
@@ -1,12 +1,12 @@
 package com.springboot.web;
 
 import com.springboot.service.posts.PostsService;
+import com.springboot.web.dto.PostsResponseDto;
 import com.springboot.web.dto.PostsSaveRequestDto;
+import com.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor //final이 붙은 필드에 대해 생성자를 자동으로 생성해줌
 @RestController
@@ -17,5 +17,16 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts") ///api/v1/posts 요청 URL로 들어오는 HTTP POST 요청을 처리하는 메소드라는 것을 의미함
     public long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    //@PathVariable Long id => HTTP URL의 {id}를 파라미터로 받아옴
+    public long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 }
